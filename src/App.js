e9b4fe955fe0fed544319ce54ecda8cc57f5639e
AllanRaf <rafale.uk@googlemail.com>
2019-09-13 14:43:07 +0200

    first photo album app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import store from './store'
+import {Provider} from 'react-redux'
 
 function App() {
+
+  //render() {
+
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
+    );
+  //}
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +40,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
